f862ab035b384049cc2678edd63dd003f3a304c6
Jordan Sommers <jtsommer@ucsc.edu>
2014-05-31 21:57:24 -0700

    Parse test functions and Javascript SDK

--- edibility-cloud/cloud/main.js
+++ edibility-cloud/cloud/main.js
@@ -2,5 +2,47 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	response.success("Hello world!");
+});
+
+Parse.Cloud.define("testpush", function(request, response) {
+	Parse.Push.send(
+		{
+			channels: [ "testchannel" ],
+			data: {
+				alert: "Test Notification Received"
+			}
+		}, 
+		{
+			success: function() {
+				// Push was successful
+				response.success("Test Push Sent");
+			},
+			error: function(error) {
+				// Handle error
+				response.error("Test Push Not Sent");
+			}
+		}
+	);
+});
+
+Parse.Cloud.job("testjob", function(request, response) {
+	Parse.Push.send(
+		{
+			channels: [ "testchannel" ],
+			data: {
+				alert: "Test Notification Received"
+			}
+		}, 
+		{
+			success: function() {
+				// Push was successful
+				response.success("Test Push Sent");
+			},
+			error: function(error) {
+				// Handle error
+				response.error("Test Push Not Sent");
+			}
+		}
+	);
 });
