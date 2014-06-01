d50ff21aa70ae3776636f41b4dfe20ea366673df
Jordan Sommers <jtsommer@ucsc.edu>
2014-05-31 17:55:36 -0700

    First pass at Java class framework needed for GSON deserialization of Kimono API data GSON: "I sure hope it works"
    API - http://www.kimonolabs.com/apis/6guup5y4

---
+++ edibility-android/src/edu/ucsc/teambacon/edibility/deserialization/MealFoodList.java
@@ -0,0 +1,9 @@
+package edu.ucsc.teambacon.edibility.deserialization;
+
+import java.util.ArrayList;
+
+public class MealFoodList {
+	public String firstItem;
+	public ArrayList<String> allFood;
+	public String lastItem;
+}
