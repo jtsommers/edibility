00bc899dcb5a1b4fd15dd137e795e9b5f23b158d
Jordan Sommers <jtsommer@ucsc.edu>
2014-06-01 23:34:52 -0700

    Sanitize the kimono string to remove the empty food/meal stuff GSON died when trying to parse allFood and allFood was an empty string not an array

--- edibility-android/src/edu/ucsc/teambacon/edibility/FoodListActivity.java
+++ edibility-android/src/edu/ucsc/teambacon/edibility/FoodListActivity.java
@@ -21,25 +21,23 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-
-
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 import edu.ucsc.teambacon.edibility.deserialization.*;
 
 public class FoodListActivity extends ActionBarActivity {
-	
+
 	private static final String DOWNLOAD_URL = "http://www.kimonolabs.com/api/6guup5y4?apikey=e9c97d5dd3b6d537d322c030e00fa7a6";
 	static HashMap<String, String> diningHalls;
 	public static final String LOG_TAG = "FOODLIST";
 
 	private ChoiceAdapter adapter;
-	public ArrayList<KimonoData> list; /// as Singleton  
+	public ArrayList<String> list; // as global
 	private BackgroundDownloader downloader;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		Log.i(LOG_TAG,"1 - oncreaete");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_food_list);
 
@@ -48,81 +46,92 @@ public class FoodListActivity extends ActionBarActivity {
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 		createLocationCodeMap();
-		list = new ArrayList<KimonoData>();
+		list = new ArrayList<String>();
 	}
 
-	
 	protected void onResume() {
 		super.onResume();
-		Log.i(LOG_TAG,"2 - onResuem");
-		
-		
+
 		// Sanity check
 		if (list == null) {
-			list = new ArrayList<KimonoData>();
-		//	global = SingletonData.getInstance(getApplicationContext());
-		}//*/
-		
+			list = new ArrayList<String>();
+			// global = SingletonData.getInstance(getApplicationContext());
+		}// */
+
 		// Set up adapter if it hasn't already been created
-		if (adapter == null) {	
+		if (adapter == null) {
 			adapter = new ChoiceAdapter(this, R.layout.activity_food_list, list);
 			((ListView) findViewById(R.id.food_listView)).setAdapter(adapter);
 		}
-		//list.size()
-		// Download and popuplate list data here, don't reset data unless list is empty
+		// list.size()
+		// Download and popuplate list data here, don't reset data unless list
+		// is empty
 		if (list.size() < 1) {
 			reloadSources();
 		}
 	}
-	
+
 	// Clear list if it exists and redownload new sources
 	public void reloadSources() {
 		// Only start a new download if previous download is not running
-		if (downloader == null || downloader.getStatus() == BackgroundDownloader.Status.FINISHED) {
+		if (downloader == null
+				|| downloader.getStatus() == BackgroundDownloader.Status.FINISHED) {
 			downloader = new BackgroundDownloader(new ChoiceCompletion());
 			downloader.execute(FoodListActivity.DOWNLOAD_URL);
 		}
 	}// mark
-	
-	
+
 	private class ChoiceCompletion extends BDCompletionTask {
 
 		@Override
 		void execute(String s) {
+			// Sanitize string to remove empty data
+			s = s.replaceAll(",\\n.*\\{\\n.*\\n.*\"allFood\": \"\",\\n.*\\n.*\\}", "");
 			Gson gson = new Gson();
-			
-			FoodItemData data = gson.fromJson(s, FoodItemData.class);
-			ArrayList<KimonoData> stuff = data.allFood;
-			// Empty old array list
-		
-			if (list == null) {
-				list = new ArrayList<KimonoData>();
-			} else {
-				list.clear();
+
+			KimonoData data = null;
+			try {
+				data = gson.fromJson(s, KimonoData.class);
+			} catch (JsonSyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		
-			Log.i(LOG_TAG," ---------------- it stops here  --------- "  );
-		//	Log.i(LOG_TAG," ----------------execute --------- " + data.allFood.size() );
-			for (int i = 0; i < stuff.size(); i++) {
-				
-				KimonoData item = stuff.get(i);
-				// Copy title from URL if URL exists but not title
-				
-				list.add(item);
-					
-			}//*/
-			
-			Log.i(LOG_TAG,"-------------- 3333 --------- " );
-			adapter.notifyDataSetChanged();
+			// TODO: This is a temporary measure to list only the first meal
+			if (data != null) {
+				ArrayList<String> stuff = data.results.food.get(0).allFood;
+				// Empty old array list
+
+				if (list == null) {
+					list = new ArrayList<String>();
+				} else {
+					list.clear();
+				}
+
+				Log.i(LOG_TAG, " ---------------- it stops here  --------- ");
+				// Log.i(LOG_TAG," ----------------execute --------- " +
+				// data.allFood.size() );
+				for (int i = 0; i < stuff.size(); i++) {
+
+					String item = stuff.get(i);
+					// Copy title from URL if URL exists but not title
+
+					list.add(item);
+
+				}// */
+
+				Log.i(LOG_TAG, "-------------- 3333 --------- ");
+				adapter.notifyDataSetChanged();
+			} else {
+				Log.d(LOG_TAG, "Data not set");
+			}
 		}
-		
+
 	}
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
-		Log.i(LOG_TAG,"4 - onMenu");
+		Log.i(LOG_TAG, "4 - onMenu");
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.food_list, menu);
 		return true;
@@ -156,7 +165,7 @@ public class FoodListActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
-	
+
 	// Populate the hashmap with dining hall codes used by FoodPro
 	private static void createLocationCodeMap() {
 		if (diningHalls == null) {
@@ -168,43 +177,44 @@ public class FoodListActivity extends ActionBarActivity {
 			diningHalls.put("porter", "25");
 		}
 	}
-	
-	private class ChoiceAdapter extends ArrayAdapter<KimonoData>{
+
+	private class ChoiceAdapter extends ArrayAdapter<String> {
 
 		int resource;
 		@SuppressWarnings("unused")
 		Context context;
-		
-		public ChoiceAdapter(Context _context, int _resource, List<KimonoData> items) {
-		
+
+		public ChoiceAdapter(Context _context, int _resource, List<String> items) {
+
 			super(_context, _resource, items);
 			resource = _resource;
 			context = _context;
 			this.context = _context;
 		}
-		
+
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			Log.i(LOG_TAG,"5 -contro");
+			Log.i(LOG_TAG, "5 -contro");
 			LinearLayout newView;
-			Log.i(LOG_TAG,"5 -contro");
-			KimonoData item = getItem(position);
-			
+			Log.i(LOG_TAG, "5 -contro");
+			String item = getItem(position);
+
 			// Inflate a new view if necessary.
 			if (convertView == null) {
 				newView = new LinearLayout(getContext());
 				String inflater = Context.LAYOUT_INFLATER_SERVICE;
-				LayoutInflater vi = (LayoutInflater) getContext().getSystemService(inflater);
-				vi.inflate(resource,  newView, true);
+				LayoutInflater vi = (LayoutInflater) getContext()
+						.getSystemService(inflater);
+				vi.inflate(resource, newView, true);
 			} else {
 				newView = (LinearLayout) convertView;
 			}
-			
+
 			// Fills in the view.
-			//TextView tv = (TextView) newView.findViewById(R.id.listText);
+			// TextView tv = (TextView) newView.findViewById(R.id.listText);
 			Button b = (Button) newView.findViewById(R.id.listButton);
-			//tv.setText(item.name);
-			//b.setText(item.name); 
+			// tv.setText(item.name);
+			// b.setText(item.name);
 			b.setText("hmmmm");
 
 			// Sets a listener for the button, and a tag for the button as well.
@@ -216,16 +226,17 @@ public class FoodListActivity extends ActionBarActivity {
 					String s = (String) v.getTag();
 					int pos = Integer.parseInt(s);
 					// Launch ReadActivity with item.url
-					KimonoData item = getItem(pos);
-				//	Log.d(LOG_TAG, item.url + " clicked");
-				//	global.urlToVisit = item.url;
-					Intent intent = new Intent(getApplicationContext(), SubscribedAlertsActivity.class);
+					String item = getItem(pos);
+					// Log.d(LOG_TAG, item.url + " clicked");
+					// global.urlToVisit = item.url;
+					Intent intent = new Intent(getApplicationContext(),
+							SubscribedAlertsActivity.class);
 					startActivity(intent);
 				}
-			}); //*/
+			}); // */
 
 			return newView;
-		}		
+		}
 	}
 
 }
