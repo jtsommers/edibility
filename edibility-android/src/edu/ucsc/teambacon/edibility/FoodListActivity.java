3491946bb28c70e8ff893d8d031a600173c9d8e4
huanghunz <huanghunz@hotmail.com>
2014-06-07 02:16:11 -0700

    1. Using the ios-like header list view for food list activity 2. Need to solve :     -The program will download a sample json if the dinning does not have food.     -The data order of lunch and dinner kimono json data is varied. The order sometimes is Lunch, Dinner,      sometimes is Dinner, Lunch

--- edibility-android/src/edu/ucsc/teambacon/edibility/FoodListActivity.java
+++ edibility-android/src/edu/ucsc/teambacon/edibility/FoodListActivity.java
@@ -15,96 +15,217 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import android.widget.Toast;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
 import edu.ucsc.teambacon.edibility.deserialization.*;
+import edu.ucsc.teambacon.edibility.headerlistview_source.*;
 
 public class FoodListActivity extends ActionBarActivity {
 
-	private static final String 
-	DOWNLOAD_URL = 
-	"http://www.kimonolabs.com/api/6guup5y4?apikey=e9c97d5dd3b6d537d322c030e00fa7a6";
-		
+	private static final String DOWNLOAD_URL = "http://www.kimonolabs.com/api/6guup5y4?apikey=e9c97d5dd3b6d537d322c030e00fa7a6";
+
 	public static final String LOG_TAG = "FOODLIST";
 
-	private ChoiceAdapter adapter;
-	public ArrayList<String> list; // as global
+	private HeaderListView list;
 	private BackgroundDownloader downloader;
-	
-	// dHall will get the dHall code, and be appended to DOWNLOAD_URL,  
-	// without dHall code, the sample data will be fetched.
+	private KimonoData data;
 	public String dHall = "&locationNum=";
-	
-	
+	public String dHallName;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_food_list);
 
 		Bundle extras = getIntent().getExtras();
-		if ( extras != null){
-			dHall += Utilities.getLocationCode(extras.getString("name"));
-		}//*/
-		
-		
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+		if (extras != null) {
+			dHallName = (extras.getString("name"));
+			dHall += Utilities.getLocationCode(dHallName);
+
 		}
-		Log.d(LOG_TAG," stop 0000000000 ");
-		list = new ArrayList<String>();
-	
-	}
 
-	protected void onResume() {
-		super.onResume();
-
-		// Sanity check
-		if (list == null) {
-			list = new ArrayList<String>();
-		}// */
-
-		// Set up adapter if it hasn't already been created
-		if (adapter == null) {
-			adapter = new ChoiceAdapter(this, R.layout.element_food_list, list);
-			((ListView) findViewById(R.id.food_listView)).setAdapter(adapter);
-		}
-		
-		// Download and popuplate list data here, don't reset data unless list
-		// is empty
-		if (list.size() < 1) {
-			reloadSources();
-		}
-	}
-
-	// Clear list if it exists and redownload new sources
-	public void reloadSources() {
-		// Only start a new download if previous download is not running
 		if (downloader == null
 				|| downloader.getStatus() == BackgroundDownloader.Status.FINISHED) {
 			downloader = new BackgroundDownloader(new ChoiceCompletion());
-			
-			downloader.execute(FoodListActivity.DOWNLOAD_URL+dHall);
+			downloader.execute(FoodListActivity.DOWNLOAD_URL + dHall);
+
+			// downloader.execute("http://www.kimonolabs.com/api/6guup5y4?apikey=e9c97d5dd3b6d537d322c030e00fa7a6");
 		}
-	}// mark
+
+	}
+
+	public void createHeaderListView() {
+
+		list = new HeaderListView(this);
+		list.setAdapter(new SectionAdapter() {
+			@Override
+			public int numberOfSections() {
+				int numMeal = data.results.food.size() + 1;
+				return numMeal;
+			}
+
+			@Override
+			public int numberOfRows(int section) {
+				int numFood = 1;
+
+				if (section > 0) {
+					String mealName = data.results.meals.get(section - 1).mealName;
+					if (mealName.contains("Lunch"))
+						numFood = data.results.food.get(section).allFood.size();
+					else if (mealName.contains("Dinner"))
+						numFood = data.results.food.get(section - 2).allFood
+								.size();
+					else // breakfast
+						numFood = data.results.food.get(section - 1).allFood
+								.size();
+				}
+
+				return numFood;
+			}
+
+			@Override
+			public Object getRowItem(int section, int row) {
+				return null;
+			}
+
+			@Override
+			public boolean hasSectionHeaderView(int section) {
+				return true;
+			}
+
+			@Override
+			public View getRowView(int section, int row, View convertView,
+					ViewGroup parent) {
+
+				if (convertView == null) {
+					convertView = (TextView) getLayoutInflater().inflate(
+							getResources().getLayout(
+									android.R.layout.simple_list_item_1), null);
+				}
+
+				if (section > 0) {
+					String mealName = data.results.meals.get(section - 1).mealName;
+
+					if (mealName.contains("Lunch"))
+						((TextView) convertView).setText(data.results.food
+								.get(section).allFood.get(row));
+
+					else if (mealName.contains("Dinner"))
+						((TextView) convertView).setText(data.results.food
+								.get(section - 2).allFood.get(row));
+
+					else
+						// if (mealName.contains("Breakfast") )
+						((TextView) convertView).setText(data.results.food
+								.get(section - 1).allFood.get(row));
+				} else {
+					((TextView) convertView).setText("Food Item");
+
+				}
+
+				return convertView;
+			}
+
+			@Override
+			public int getSectionHeaderViewTypeCount() {
+
+				return 2;
+			}
+
+			@Override
+			public int getSectionHeaderItemViewType(int section) {
+				return section % 2;
+			}
+
+			@Override
+			public View getSectionHeaderView(int section, View convertView,
+					ViewGroup parent) {
+
+				if (convertView == null) {
+					convertView = (TextView) getLayoutInflater().inflate(
+							getResources().getLayout(
+									android.R.layout.simple_list_item_1), null);
+				}
+
+				switch (section) {
+				case 0:
+					convertView.setBackgroundColor(getResources().getColor(
+							R.color.holo_red_light));
+					((TextView) convertView).setText(dHallName);
+					break;
+				case 1:
+					convertView.setBackgroundColor(getResources().getColor(
+							R.color.holo_orange_light));
+					((TextView) convertView).setText(data.results.meals.get(0).mealName);
+					break;
+				case 2:
+					((TextView) convertView).setText(data.results.meals.get(1).mealName);
+					convertView.setBackgroundColor(getResources().getColor(
+							R.color.holo_green_light));
+					break;
+				case 3:
+					convertView.setBackgroundColor(getResources().getColor(
+							R.color.holo_blue_light));
+					((TextView) convertView).setText(data.results.meals.get(2).mealName);
+					break;
+				}
+				return convertView;
+			}
+
+			@Override
+			public void onRowItemClick(AdapterView<?> parent, View view,
+					int section, int row, long id) {
+				super.onRowItemClick(parent, view, section, row, id);
+
+				if (section == 0)
+					return;
+
+				// ///// ==============
+				Intent intent = new Intent(getApplicationContext(),
+						SubscribedAlertsActivity.class);
+				startActivity(intent);
+
+				// ///// ===============
+
+				// Toast.makeText(FoodListActivity.this, "Section " + section +
+				// " Row " + row, Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		setContentView(list);
+
+	}
+
+	/*
+	 * protected void onResume() { super.onResume();
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
 
 	private class ChoiceCompletion extends BDCompletionTask {
 
 		@Override
 		void execute(String s) {
 			// Sanitize string to remove empty data
-		
-			s = s.replaceAll(",\\n.*\\{\\n.*\\n.*\"allFood\": \"\",\\n.*\\n.*\\}", "");
+
+			s = s.replaceAll(
+					",\\n.*\\{\\n.*\\n.*\"allFood\": \"\",\\n.*\\n.*\\}", "");
 			Gson gson = new Gson();
 
-			KimonoData data = null;
+			// KimonoData data = null;
+			data = null;
 			try {
 				data = gson.fromJson(s, KimonoData.class);
 			} catch (JsonSyntaxException e) {
@@ -112,54 +233,7 @@ public class FoodListActivity extends ActionBarActivity {
 				e.printStackTrace();
 			}
 
-
-			
-			// TODO: This is a temporary measure to list only the first meal
-			if (data != null) {
-				ParseResults foodList = data.results;	
-
-				int numMeals = foodList.food.size();
-		
-				ArrayList <String> item = new ArrayList<String>();
-				
-				if (list == null) {
-					list = new ArrayList<String>();
-				} else {
-					list.clear();
-				}
-			
-				if ( numMeals  > 1){ 
-					item = foodList.food.get(numMeals-1).allFood;
-					list.add(foodList.meals.get(numMeals-2).mealName);
-					for ( int i = 0; i < item.size(); i++){
-						list.add(item.get(i));
-					}
-						
-					item = foodList.food.get(numMeals-2).allFood;
-					list.add(foodList.meals.get(numMeals-1).mealName);
-					for ( int i = 0; i < item.size(); i++){
-						list.add(item.get(i));
-					}
-					if (numMeals > 2){
-						item = foodList.food.get(0).allFood;
-						list.add(0,foodList.meals.get(0).mealName);
-						for ( int i = 0; i < item.size(); i++){
-							list.add(1,item.get(i));
-						}
-					}	
-				}
-				else{ // only one meal
-					item = foodList.food.get(0).allFood;
-					list.add(0,foodList.meals.get(0).mealName);
-					for ( int i = 0; i < item.size(); i++){
-						list.add(item.get(i));
-					}	
-				}
-					
-				adapter.notifyDataSetChanged();
-			} else {
-				Log.d(LOG_TAG, "Data not set");
-			}
+			createHeaderListView();
 		}
 
 	}
@@ -198,70 +272,6 @@ public class FoodListActivity extends ActionBarActivity {
 			View rootView = inflater.inflate(R.layout.fragment_food_list,
 					container, false);
 			return rootView;
-		}
-	}
-
-	private class ChoiceAdapter extends ArrayAdapter<String> {
-
-		int resource;
-		@SuppressWarnings("unused")
-		Context context;
-
-		public ChoiceAdapter(Context _context, int _resource, List<String> items) {
-
-			super(_context, _resource, items);
-			resource = _resource;
-			context = _context;
-			this.context = _context;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-	
-			LinearLayout newView;
-	
-		//	String item = getItem(position);
-			String item = getItem(position);
-
-			// Inflate a new view if necessary.
-			if (convertView == null) {
-				newView = new LinearLayout(getContext());
-				String inflater = Context.LAYOUT_INFLATER_SERVICE;
-				LayoutInflater vi = (LayoutInflater) getContext()
-						.getSystemService(inflater);
-				vi.inflate(resource, newView, true);
-			} else {
-				newView = (LinearLayout) convertView;
-			}
-
-			Button b = (Button) newView.findViewById(R.id.listButton);
-		
-			b.setText(item.toString());
-			
-			// Sets a listener for the button, and a tag for the button as well.
-			b.setTag(Integer.toString(position));
-			b.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					// React to button according to tag/index
-					String s = (String) v.getTag();		
-					int pos = Integer.parseInt(s);		
-					
-					String item = getItem(pos); // item is the name of food
-
-					// if the name matches the header, do nothing
-					if ( item.contains("Lunch") || item.contains("Breakfast") ||
-						   item.contains("Dinner")){
-						return;
-					}
-
-					Intent intent = new Intent(getApplicationContext(),
-							SubscribedAlertsActivity.class);
-					startActivity(intent);
-				}
-			}); // */
-
-			return newView;
 		}
 	}
 
