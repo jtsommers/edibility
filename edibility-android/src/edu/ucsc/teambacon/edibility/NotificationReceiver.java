de5cd650ff14e1d288e654550c91bfcabc8455cf
huanghunz <huanghunz@hotmail.com>
2014-06-08 03:04:36 -0700

    self define notification

--- edibility-android/src/edu/ucsc/teambacon/edibility/NotificationReceiver.java
+++ edibility-android/src/edu/ucsc/teambacon/edibility/NotificationReceiver.java
@@ -5,15 +5,20 @@ import java.util.Iterator;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.util.Log;
 import android.widget.Toast;
 
 public class NotificationReceiver extends BroadcastReceiver {
 	
-	
+	public static final String LOG_TAG = "NotificationReceiver";
 	public static final String PARSE_DATA_KEY         =   "com.parse.Data";
 	public static final String PARSE_JSON_CHANNEL_KEY       =   "com.parse.Channel";
 
@@ -24,18 +29,43 @@ public class NotificationReceiver extends BroadcastReceiver {
 	 public void onReceive(Context context, Intent intent) {
 		 
 		 Toast.makeText(context, "Push received!",Toast.LENGTH_SHORT).show();
-	      String action = intent.getAction();
-	         String channel = intent.getExtras().getString(PARSE_JSON_CHANNEL_KEY);
-	         try {
-				JSONObject json = new JSONObject(intent.getExtras().getString(PARSE_DATA_KEY));
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-	     }
-
-	 	
-	
+		 try {
+	            String action = intent.getAction();
+	            String channel = intent.getExtras().getString( PARSE_JSON_CHANNEL_KEY );
+	            JSONObject json = new JSONObject(intent.getExtras().getString(PARSE_DATA_KEY ));
+	            String title = "New alert!";
+	            if (json.has("header"))
+	                title = json.getString("header");
+	            generateNotification(context,  title);
+	        } catch (Exception e) {
+	            Log.d(LOG_TAG, "JSONException: " + e.getMessage());
+	        }
 
 	}
+	 
+	 public static void generateNotification(Context context,  String message) {
+	        // Show the notification
+	        long when = System.currentTimeMillis();
+	        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+	       // Notification notification = new Notification(icon, message, when);
+	        
+	        Notification notification = new Notification(R.drawable.ic_launcher, message,when);
+	        
+	        
+	        String title = context.getString(R.string.app_name);
+	        Intent notificationIntent = new Intent(context, NotificationReceiver.class);
+
+	        // set intent so it does not start a new activity
+	        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+	        PendingIntent intent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+	        notification.setLatestEventInfo(context, title, message, intent);
+	        notification.vibrate = new long[] { 500, 500 };
+	        notification.sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+	        notification.flags = 
+	            Notification.FLAG_AUTO_CANCEL | 
+	            Notification.FLAG_SHOW_LIGHTS;
+
+	        notificationManager.notify(0, notification);
+	    }
+}
