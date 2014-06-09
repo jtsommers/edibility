491f537dbbf016fc3a17b9a8895d5fed49e3ebfc
Jordan Sommers <jtsommer@ucsc.edu>
2014-06-08 17:50:01 -0700

    Refactored notifications to use a compatibility library, launches main application screen

--- edibility-android/src/edu/ucsc/teambacon/edibility/NotificationReceiver.java
+++ edibility-android/src/edu/ucsc/teambacon/edibility/NotificationReceiver.java
@@ -1,12 +1,8 @@
 package edu.ucsc.teambacon.edibility;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
-import org.json.JSONException;
 import org.json.JSONObject;
-
-
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -15,69 +11,81 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.widget.Toast;
 
 public class NotificationReceiver extends BroadcastReceiver {
-	
-	
-	public static final String LOG_TAG = "NotificationReceiver";
-	public static final String PARSE_DATA_KEY         =   "com.parse.Data";
-	public static final String PARSE_JSON_CHANNEL_KEY       =   "com.parse.Channel";
-	
-	 @Override
-	 public void onReceive(Context context, Intent intent) {
-		 
 
-	     ArrayList<FoodItem> foodlist = SavedPreferences.getInstance().getSavedFoodList();
-		 FoodItem foodItem;
-		 String saved = "";
-		 
-		//Toast.makeText(context, "Push received!",Toast.LENGTH_SHORT).show();
-		 try {
-	            String action = intent.getAction();
-	            String channel = intent.getExtras().getString( PARSE_JSON_CHANNEL_KEY );
-	            
-	            for (int i = 0; i < foodlist.size(); i ++){
-	            	foodItem = foodlist.get(i);
-	            	
-	            	if (channel.indexOf(foodItem.channelName) != -1)	{
-	            		saved = foodItem.displayName;
-	            	}
-	            }
-	           
-	            JSONObject json = new JSONObject(intent.getExtras().getString(PARSE_DATA_KEY ));
-	        
-	           // String var = json.getString("header"); 
-	            String college =  Utilities.getStringResourceByName(json.getString("college"));
-	          
-	            //TODO : a better message maybe
-	            String message = college + " now has " + saved;
-	            
-	            // Calls a function to define the notification ( rington,content);
-	            defineNotification(context,  message);
-	        } catch (Exception e) {
-	            Log.d(LOG_TAG, "JSONException: " + e.getMessage());
-	        }
+	public static final String LOG_TAG = "NotificationReceiver";
+	public static final String PARSE_DATA_KEY = "com.parse.Data";
+	public static final String PARSE_JSON_CHANNEL_KEY = "com.parse.Channel";
+	public static String college = "";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		ArrayList<FoodItem> foodlist = SavedPreferences.getInstance()
+				.getSavedFoodList();
+		FoodItem foodItem;
+		String saved = "";
+
+		// Toast.makeText(context, "Push received!",Toast.LENGTH_SHORT).show();
+		try {
+			String action = intent.getAction();
+			String channel = intent.getExtras().getString(
+					PARSE_JSON_CHANNEL_KEY);
+
+			for (int i = 0; i < foodlist.size(); i++) {
+				foodItem = foodlist.get(i);
+
+				if (channel.indexOf(foodItem.channelName) != -1) {
+					saved = foodItem.displayName;
+				}
+			}
+
+			JSONObject json = new JSONObject(intent.getExtras().getString(
+					PARSE_DATA_KEY));
+
+			// String var = json.getString("header");
+			college = Utilities.getStringResourceByName(json
+					.getString("college"));
+
+			// TODO : a better message maybe
+			String message = college + " now has " + saved;
+
+			// Calls a function to define the notification ( rington,content);
+			defineNotification(context, message);
+		} catch (Exception e) {
+			Log.d(LOG_TAG, "JSONException: " + e.getMessage());
+		}
 
 	}
-	 
-	 public static void defineNotification(Context context,  String message) {
+
+	public static void defineNotification(Context context,  String message) {
 	        // Show the notification
 	        long when = System.currentTimeMillis();
 	        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 	        
-	        //TODO - change square-shape launching icon
-	        Notification notification = new Notification(R.drawable.ic_launcher, message,when);
-	       
-	        String title = context.getString(R.string.app_name);
 	        
-	        Intent notificationIntent = new Intent(context, NotificationReceiver.class);
+	        Notification notification;
+	        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+	        builder.setContentText(message)
+	        		.setContentTitle(context.getString(R.string.app_name))
+	        		.setSmallIcon(R.drawable.ic_launcher)
+	        		.setWhen(when);
+	        
+	        // Build an intent to launch from notification
+	        Intent notificationIntent = new Intent(context, LocationSelectionActivity.class);
+//	        notificationIntent.putExtra("name", college);
+//	        notificationIntent.setAction(EdibilityApplication.ACTION_FOOD_LIST);
 
 	        // set intent so it does not start a new activity
-	        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-	        PendingIntent intent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
-	        notification.setLatestEventInfo(context, title, message, intent);
+	        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+	        PendingIntent intent = PendingIntent.getActivity(context, 0, notificationIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
+	        // Finish building notification
+	        builder.setContentIntent(intent);
+	        notification = builder.build();
+	        
 	        notification.vibrate = new long[] { 500, 500 };
 	        notification.sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
