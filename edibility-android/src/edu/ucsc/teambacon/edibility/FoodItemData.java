77512e3936bab6f63754b7003b890f74f8dfdb62
huanghunz <huanghunz@hotmail.com>
2014-06-08 18:17:04 -0700

    Deleted unused class

--- edibility-android/src/edu/ucsc/teambacon/edibility/FoodItemData.java
+++
@@ -1,9 +0,0 @@
-package edu.ucsc.teambacon.edibility;
-
-import java.util.ArrayList;
-
-import edu.ucsc.teambacon.edibility.deserialization.KimonoData;
-
-public class FoodItemData {
-	public ArrayList<KimonoData> allFood;
-}
